cc2ed5776e49f8c7370d762edbc3858e14fcc64f
kristynerhaugen <kristynerhaugen@gmail.com>
2022-08-03 09:40:59 -0500

    started login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,11 +6,11 @@ const { Post, User, Comment } = require("../models");
 router.get("/", (req, res) => {
   console.log("=========================");
   Post.findAll({
-    attributes: ["id", "title", "postContent"],
+    attributes: ["id", "title", "postContent", "createdAt"],
     include: [
       {
         model: Comment,
-        attributes: ["id", "commentText", "post_id", "user_id"],
+        attributes: ["id", "commentText", "post_id", "user_id", "createdAt"],
         include: {
           model: User,
           attributes: ["username"],
@@ -42,11 +42,11 @@ router.get("posts/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "title", "postContent"],
+    attributes: ["id", "title", "postContent", "createdAt"],
     include: [
       {
         model: Comment,
-        attributes: ["id", "commentText", "post_id", "user_id"],
+        attributes: ["id", "commentText", "post_id", "user_id", "createdAt"],
         include: {
           model: User,
           attributes: ["username"],
