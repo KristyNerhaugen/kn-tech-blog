adee129686ed58d67a7b961465e5c2d168968823
kristynerhaugen <kristynerhaugen@gmail.com>
2022-08-03 13:22:19 -0500

    dashboard route started and dashboard handlebars set up

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+// get route to get all posts on dashboard
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "postContent", "createdAt"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "commentText", "post_id", "user_id", "createdAt"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
