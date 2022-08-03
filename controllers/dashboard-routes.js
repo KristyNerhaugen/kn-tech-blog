ee704334c2a5adc3c846bfceecfbab500441baf6
kristynerhaugen <kristynerhaugen@gmail.com>
2022-08-03 16:00:40 -0500

    created edit-post handlebars and updated dashboard-routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -36,4 +36,39 @@ router.get("/", withAuth, (req, res) => {
     });
 });
 
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: ["id", "title", "postContent", "createdAt"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "commentText", "post_id", "user_id", "createdAt"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render("edit-post", {
+          post,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
