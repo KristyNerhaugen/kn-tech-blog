f960a93586a378bae60eb694779f98107f9753fa
kristynerhaugen <kristynerhaugen@gmail.com>
2022-08-03 15:31:38 -0500

    post js set up

---
+++ public/javascript/add-post.js
@@ -0,0 +1,28 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const postContent = document.querySelector('input[name="postContent"]').value;
+
+  const response = await fetch("/api/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      postContent,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+// executes when submit button for create post is clicked
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
